4dcb69f247e9413e5e9f57929c5f49ecc3e17e4a
krishnendu basak <krisauz17@yahoo.com>
2022-06-01 11:07:56 +1000

    programming-univbasics-4-building-nested-hashes-code-along is done

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,7 +6,9 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-      ]
+        {:name => "Benvolio", :age => "17", :attitude => "worried"},
+        {:name => "Mercuito", :age => "18", :attitude => "hot-headed"}
+      ] 
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
@@ -14,10 +16,9 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {:name => "Steven", :age => "30", :attitude => "confused"},
+        {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
-
-  
-
 end
